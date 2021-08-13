308a08a80b0ba026b02ae0c03d8bf3a30fbf3504
italo-rodrigues1 <italosport110@hotmal.com>
2021-08-13 11:17:51 -0300

    organizando as routes

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -4,60 +4,27 @@ const usuarioController = require('../controllers/usuarioControllers');
 const autorController = require('../controllers/autorControllers');
 const editoraController = require('../controllers/editoraControllers');
 const emailControllers = require('../controllers/emailControllers')
-
-
 const routes = Router(); 
 
 routes.get('/', (req, res) => {
     res.send('API Gamaxpfinal!')
 })
 
-routes.post('/send',emailControllers.sendEmail);
-
 routes.get('/livro/faixaetaria/:faixaetaria', livroController.getFaixaEtaria);
-
 routes.get('/livro/:id', livroController.getOneId);
-
 routes.get('/livro', livroController.getAll);
-
 routes.get('/usuario', usuarioController.getUsuario);
 
-
+routes.post('/send',emailControllers.sendEmail);
 routes.post('/livro', livroController.createLivros);
 routes.post('/autor', autorController.createAutor);
 routes.post('/editora',editoraController.createEditora);
 routes.post('/usuario',usuarioController.createUsuario);
 
-
-
-
-
 routes.put('/livro/:id', livroController.update);//PRONTO
-
 
 routes.delete('/livro/:id', livroController.delete);//PRONTO
 
-
-
-
 module.exports = routes;
 
- // const transporte = nodemailer.createTransport({
-    //   host:"smtp.sendgrid.net",
-    //   port: 587,
-    //   auth:{ User , Pass}
-    // }) 
-
-    // transporte.sendMail({
-    //   from: "italosport110@hotmail.com",
-    //   to: "italosport110@hotmail.com",
-    //   replyTo: 'italosport110@hotmail.com',
-    //   subject: 'Deu certo inhoggg!!!',
-    //   text: 'Oi, tudo bem?',
-    //   html: '<p>Oi, tudo bem?</p>'
-      
-    // }).then((info) => {
-    //   res.send(info)
-    // }).catch(error => {
-    //   res.send(error)
-    // })
+ 
